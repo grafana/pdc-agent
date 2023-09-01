2ad6c5e906785d386efacd523bd5bdf647c752f3
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-09-01 10:49:16 -0300

    check if network exists in KeyManager.argumentsHash

--- pkg/ssh/keymanager.go
+++ pkg/ssh/keymanager.go
@@ -209,7 +209,10 @@ func (km KeyManager) argumentsHashIsDifferent(hash string) bool {
 
 // argumentsHash returns a hash of the values that end up in the principals field of the certificate.
 func (km KeyManager) argumentsHash() string {
-	value := fmt.Sprintf("%s/%s", km.cfg.PDC.HostedGrafanaID, km.cfg.PDC.Network)
+	value := km.cfg.PDC.HostedGrafanaID
+	if km.cfg.PDC.Network != "" {
+		value = fmt.Sprintf("%s/%s", value, km.cfg.PDC.Network)
+	}
 	return fmt.Sprintf("%x", sha256.Sum256([]byte(value)))
 }
 
