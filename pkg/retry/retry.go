07587444c2164d43b30130f92cf337dec855ed77
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-11-08 10:23:08 -0300

    add comment to retry.Forever

--- pkg/retry/retry.go
+++ pkg/retry/retry.go
@@ -12,6 +12,8 @@ type Opts struct {
 	InitialBackoff time.Duration
 }
 
+// Calls a function until it succeeds, waiting an exponentially increasing amount of time between calls.
+// An initial backoff of 0 means the waiting time does not increase exponentially (useful for testing).
 func Forever(opts Opts, f func() error) {
 	attempt := 1
 
